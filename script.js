67f0a334ce7b224ed42febd0636e58dc499e6e78
Dmitry Gorbach <89874530087@mail.ru>
2022-05-14 15:21:58 +0200

    lesson05

--- script.js
+++ script.js
@@ -1,25 +1,72 @@
 "use strict";
-let title = prompt("Название проекта");
-let newArr;
+//1
+/* let arr = {};
 
-const func = function (str) {
-  if (str !== null) {
-    if (!isNaN(parseFloat(str)) && isFinite(str)) {
-      title = prompt("Передана не строка");
-      func(title);
-    } else {
-      title = title.trim().split("");
-      console.log(title);
-      if (title.length > 30) {
-        for (let i = title.length; i > 30; i--) {
-          delete title[i];
-        }
-        title = title.join("");
-      }
-      alert(title + "...");
-      console.log(typeof title, title.length, title);
+for (let i = 0; i < 7; i++) {
+  arr[i] = Math.random() * (1000 - 1) + 1;
+  arr[i] = Math.round(arr[i]);
+  arr[i] = String(arr[i]);
+
+  if (arr[i].split("")[0] === "2" || arr[i].split("")[0] === "4") {
+    console.log(arr[i]);
+  }
+}
+
+console.log("Весь массив", arr); */
+
+/* function isPrime(num) {
+  for (let i = 2; i <= num; i++) {
+    if (num % i === 0) {
+      return false;
     }
   }
-};
+  return num > 1;
+}
 
-func(title);
+function getPrimes(num) {
+  const primes = [];
+
+  for (let i = 2; i <= num; i++) {
+    if (isPrime(i)) {
+      primes.push(i);
+    }
+  }
+
+  return primes;
+}
+
+console.log(getPrimes(120)); */
+
+//function declaration
+// Можно запустить перед определением !!!
+testFunction();
+
+function testFunction(/* аргументы */) {
+  const x = 5;
+  const y = 5;
+  console.log(x + 5);
+}
+
+/* Если надо сохранить значение переменнйо после отработки функции , то эту переменную надо указывать вне функции , или создавать кажыдй раз внутри функции
+ Пр:  */
+
+let num = 9;
+
+function count() {
+  num++;
+  console.log(num);
+}
+
+count(); //10
+count(); //11
+count(); //12
+
+function coun2() {
+  let d = 9;
+  d++;
+  console.log(d);
+}
+
+coun2();
+coun2();
+coun2();
